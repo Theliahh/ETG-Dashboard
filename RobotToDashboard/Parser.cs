81ac42e4f88262647a9466171b83d95cdcca6473
Garrett Musto <garrettmusto@gmail.com>
2016-05-26 15:33:36 -0400

    Added parser example

--- RobotToDashboard/Parser.cs
+++ RobotToDashboard/Parser.cs
@@ -13,10 +13,39 @@ namespace RobotToDashboard
         //Parses XML to C# class
         public Parser()
         {
-            Console.WriteLine("test");
+            StringBuilder output = new StringBuilder();
+
+            //XML Parsing example
+            const string xmlString =
+                   @"<bookstore>
+                        <book genre='autobiography' publicationdate='1981-03-22' ISBN='1-861003-11-0'>
+                            <title>The Autobiography of Benjamin Franklin</title>
+                            <author>
+                                <first-name>Benjamin</first-name>
+                                <last-name>Franklin</last-name>
+                            </author>
+                            <price>8.99</price>
+                        </book>
+                    </bookstore>";
+            using (XmlReader reader = XmlReader.Create(new StringReader(xmlString)))
+            {
+                XmlWriterSettings ws = new XmlWriterSettings();
+                ws.Indent = true;
+                reader.ReadToFollowing("book");
+                reader.MoveToFirstAttribute();
+                string genre = reader.Value;
+                output.AppendLine("The genre value: " + genre);
+
+                reader.ReadToFollowing("title");
+                output.AppendLine("Content of the title element: " + reader.ReadElementContentAsString());
+                reader.ReadToFollowing("price");
+                output.AppendLine("Price of the book: " + reader.ReadElementContentAsString());
+            }
+            Console.Write(output.ToString());
+            Console.Read();
         }
 
-        void parseRobotXML()
+        private void ParseRobotXml()
         {
 
         }
